da74464b9a6e9549e17b167bc2d7d75d9c7814b0
Brandon McMullin <brandon.m.mcmullin@gmail.com>
2013-01-23 09:21:44 -0800

    fixing object design
    so they can no de-serialize properly and maintain prototype based
    inheritance

--- Bnet-Companion/types/bc-bnetClient.js
+++ Bnet-Companion/types/bc-bnetClient.js
@@ -1,27 +1,22 @@
 (function() {
 	var $j = jQuery.noConflict();
-	var devMode = false;
+	var devMode = true;
 	var _newsFeed = null;	
-	var _testFeed = null;
 	var _bungieRssUrl = 'http://www.bungie.net/en-us/Rss/News';
 	
 	BcBnetClient = function() {
-		//if ( localStorage.newsFeed ) {
-		//	_testFeed = new BCNewsList(JSON.parse(localStorage.newsFeed));
-		//	console.log(_testFeed);
-		//} else {
-			_testFeed = new BCNewsList();
-		//}
+		if ( localStorage.newsFeed ) {
+			_newsFeed = new BCNewsList(JSON.parse(localStorage.newsFeed));
+		} else {
+			_newsFeed = new BCNewsList();
+		}
 		
 		updateNews();
 	};
 	
 	BcBnetClient.prototype.getNews = function() {
-		if ( devMode ) {
-			return getNewsFeedMock();
-		} else {
-			return _testFeed;
-		}
+	
+			return _newsFeed;
 		
 	};
 	
@@ -30,10 +25,21 @@
 	};
 	
 	var updateNews = function() {
-		getTwitterFeed();
-		getBungieBlog();
+	
+		if ( devMode ) {
+			_newsFeed = getNewsFeedMock();
+		} else {
+			try {
+				getTwitterFeed();
+				getBungieBlog();
+			} catch ( e ) {
+				console.log('An exception occurred when attempting to fetch the news feed:');
+				console.log(e);
+			}
+			
+		}
 				
-		localStorage.newsFeed = JSON.stringify(_testFeed);
+		localStorage.newsFeed = JSON.stringify(_newsFeed);
 		
 		// detect and store latest news item
 		/*if ( localStorage.latestPubDate && _newsFeed[0] ) {
@@ -72,9 +78,9 @@
 					
 					createdAt = $j(this).find('pubDate').text().replace("+0000", "GMT");
 					
-					item = new BCNewsItem(title, link, createdAt, 'bnet');
+					item = new BCNewsItem({title:title, url:link, pubDate:createdAt, source:'bnet'});
 					
-					_testFeed.add(item);
+					_newsFeed.add(item);
 				});
 			}
 		});
@@ -82,7 +88,6 @@
 		return bungieBlog;
 	};
 	
-
 	var getTwitterFeed = function() {
 		var feedData = new Array();
 		
@@ -100,23 +105,22 @@
 				//title, url, pubDate, source, itemId
 					link = "http://twitter.com/bungie/statuses/" + data[i].id_str;
 					
-					item = new BCNewsItem(data[i].text, link, data[i].created_at, 'twitter', data[i].id_str);
-					_testFeed.add(item);
+					item = new BCNewsItem({title:data[i].text, url:link, pubDate:data[i].created_at, source:'twitter', itemId:data[i].id_str});
+					_newsFeed.add(item);
 				}
 			}
 		
 		});
 	}
 	
-	var sortFeed = function(feed) {
-		return feed.sort(function(a,b) {
-			return (new Date(b._pubDate) - new Date(a._pubDate));
-		});
-	}
-	
+	var getYoutubeFeed = function() {};
 	
 	var getNewsFeedMock = function() { 
-		return [new BCNewsItem('Bungie has released Bungie.next to beta!', 'http://www.bungie.net', 'Wed Jan 16 00:42:25 +0000 2013', 'twitter', 'g7g7g7'), new BCNewsItem('Bungie has released Bungie.next to beta!', 'http://www.bungie.net', 'Wed Jan 16 00:42:25 +0000 2013', 'twitter', 'g7g7g7')];
+		var mockList = new BCNewsList();
+		mockList.add(new BCNewsItem({title:'Bungie has released Bungie.next to beta!', url:'http://www.bungie.net', pubDate:'Wed Jan 16 00:42:25 +0000 2013', source:'twitter', itemId:'g7g7g7'}));
+		mockList.add(new BCNewsItem({title:'Se7enty 7', url:'http://www.bungie.net', pubDate:'Wed Jan 16 09:42:25 +0000 2013', source:'bnet'}));
+		return mockList;
+	
 	}
 
 })();
--- Bnet-Companion/types/newsItem.js
+++ Bnet-Companion/types/newsItem.js
@@ -1,63 +1,48 @@
 // news item
 (function() {	
-	BCNewsItem = function(title, url, pubDate, source, itemId) {
-		this._title = title;	
-		this._url = url;
-		this._pubDate = pubDate;
-		this._source = source;
-		
+	BCNewsItem = function(itemHash) {
+		this.title = null;	
+		this.url = null;
+		this.pubDate = null;
+		this.source = null;		
 		this.itemId = null;
+		this.thumbnail = null;
 		
-		if ( itemId ) {
-			this._itemId = itemId;
-		}
+		this.title = itemHash.title;
+		this.url = itemHash.url;
+		this.pubDate = itemHash.pubDate;
+		this.source = itemHash.source;
 		
-		this._thumbnail = "images/" + source + '.gif';
+		this.thumbnail = 'images/' + this.source + '.gif';
 		
-		this.render = function(parent) {
-			// set up the containing LI
-			var newsItemBox = parent.appendChild(document.createElement('li'));
-			newsItemBox.className = 'bc-news-item';
-			newsItemBox.setAttribute('data-item-link', this._url);
-			
-			// now add the image
-			var itemImg = newsItemBox.appendChild(document.createElement('img'));
-			itemImg.src = this._thumbnail;
-			
-			// render the title
-			var itemText = newsItemBox.appendChild(document.createElement('p'));
-			itemText.className = 'bc-news-details';
-			itemText.innerText = this._title.substring(0,60).concat('...');
-			
-			// lastly, the pubdate
-			var itemDate = newsItemBox.appendChild(document.createElement('span'));
-			itemDate.className = 'bc-news-pub-date';
-			itemDate.innerText = this._pubDate;
-		};
+		if ( itemHash.itemId ) {
+			this.itemId = itemHash.itemId;
+		} 
+		
 	};
 		
 	// Creates and wires the news ite on the page
-	/*BCNewsItem.prototype.render = function(parent) {
+	BCNewsItem.prototype.render = function(parent) {
 	
 		// set up the containing LI
 		var newsItemBox = parent.appendChild(document.createElement('li'));
 		newsItemBox.className = 'bc-news-item';
-		newsItemBox.setAttribute('data-item-link', this._url);
+		newsItemBox.setAttribute('data-item-link', this.url);
 		
 		// now add the image
 		var itemImg = newsItemBox.appendChild(document.createElement('img'));
-		itemImg.src = this._thumbnail;
+		itemImg.src = this.thumbnail;
 		
 		// render the title
 		var itemText = newsItemBox.appendChild(document.createElement('p'));
 		itemText.className = 'bc-news-details';
-		itemText.innerText = this._title.substring(0,60).concat('...');
+		itemText.innerText = this.title.substring(0,60).concat('...');
 		
 		// lastly, the pubdate
 		var itemDate = newsItemBox.appendChild(document.createElement('span'));
 		itemDate.className = 'bc-news-pub-date';
-		itemDate.innerText = this._pubDate;
-	};*/
+		itemDate.innerText = this.pubDate;
+	};
 	
 })();
 
@@ -69,7 +54,13 @@
 		
 		if ( storedList ) {
 			this.keys = storedList.keys;
-			this.items = storedList.items;
+			for (var item in storedList.items) {
+				
+				if ( storedList.items.hasOwnProperty(item) ) {
+					
+					this.items[item] = new BCNewsItem(storedList.items[item]);
+				} 
+			}
 		}
 		
 	};
@@ -78,19 +69,13 @@
 		return this.keys.sort(sortKeys);
 	};
 	
-	BCNewsList.prototype.speak = function(msg) {
-		console.log(msg);
-	};
-	
 	BCNewsList.prototype.add = function(item) {
-		var key = item._source[0] + '-';
-		key += item._pubDate;
+		var key = item.source[0] + '-';
+		key += item.pubDate;
 		
 		if ( !this.items[key] ) {
 			this.items[key] = item;
 			this.keys.push(key);
-		
-			console.log(this.items[key]._title);
 		}
 	};
 	
