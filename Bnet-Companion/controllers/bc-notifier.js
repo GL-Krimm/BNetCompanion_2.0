834d971ebdba6e6aa092344a73cc78cb109342d0
Brandon McMullin <brandon.m.mcmullin@gmail.com>
2013-02-14 09:45:36 -0800

    adding more to notification system
    styled and added basic behavior logic to the news notifier type. Next
    steps are to move it into a background page with appropriate CSS, and to
    decide how clicking the item should behave.

--- Bnet-Companion/controllers/bc-notifier.js
+++ Bnet-Companion/controllers/bc-notifier.js
@@ -3,16 +3,40 @@
 */
 
 (function() {
-	var _sandbox = null;
+	var $j = jQuery.noConflict();
 	
 	BcNotifierWidget = function(sandbox) {
 		//_sandbox = sandbox;
 	};
 	
 	BcNotifierWidget.prototype.notify = function() {
-		var notification = document.createElement('span');
-		notification.textContent = 'Hello, World!';
+		var notification = document.createElement('div');
+		notification.className = 'bc-notification';
+		
+		var closeBtn = notification.appendChild(document.createElement('span'));
+		closeBtn.textContent = 'X';
+		closeBtn.className = ' bc-close-notification-btn ';
+		
+		var virgil = notification.appendChild(document.createElement('img'));
+		virgil.src = 'images/bnet.gif';
+		
+		var msg = notification.appendChild(document.createElement('p'));
+		msg.textContent = 'New items detected...';
+		
 		document.body.insertBefore(notification, document.body.firstChild);
+		
+		$j(closeBtn).click(function(e) {
+			$j(notification).fadeOut('slow').promise(function() {
+				$j(notification).remove();
+			});
+		});
+		
+		setTimeout(function() {
+			$j(notification).fadeOut('slow').promise(function() {
+				$j(notification).remove();
+			});
+		}, 4000);
+		
 	};
 	
 })();
